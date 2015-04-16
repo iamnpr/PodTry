738e8309cc17f7b3af2044f17a97cd3518b23c45
Naveen Prasad Raja <iamnpr@outlook.com>
2015-04-15 21:46:01 -0700

    another commit

---
+++ PodTry/PodTry/DemoView.swift
@@ -0,0 +1,21 @@
+//
+//  DemoView.swift
+//  PodTry
+//
+//  Created by Raja, Naveen (ETW) on 4/15/15.
+//  Copyright (c) 2015 iamnpr. All rights reserved.
+//
+
+import UIKit
+
+class DemoView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
